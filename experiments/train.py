d10f0d91b5b30b6bde45b7035860a11e9b69b97b
Zac Letian Chen <zac.letian.chen@gmail.com>
2019-05-17 20:44:57 -0400

    change clip mean and covariance to tanh activation in the last layer

--- experiments/train.py
+++ experiments/train.py
@@ -65,7 +65,7 @@ def mlp_model_policy(input, num_outputs, scope, reuse=False, num_units=64, rnn_c
         out = input
         out = layers.fully_connected(out, num_outputs=num_units, activation_fn=tf.nn.leaky_relu)
         out = layers.fully_connected(out, num_outputs=num_units, activation_fn=tf.nn.leaky_relu)
-        out = layers.fully_connected(out, num_outputs=num_outputs, activation_fn=None)
+        out = layers.fully_connected(out, num_outputs=num_outputs, activation_fn=tf.nn.tanh)
         return out
 
 
